2f4661fb35977107754831dbca9a0abefb586cff
RusuGabriel <rusugabriel13@gmail.com>
2020-03-21 14:25:21 +0200

    beta version

--- src/utils.c
+++ src/utils.c
@@ -2,37 +2,164 @@
 
 SO_FILE *so_fopen(const char *pathname, const char *mode)
 {
-    SO_FILE *file_stream = NULL;
-    create(&file_stream, mode);
-    file_stream->descriptor = open(pathname, file_stream->flags, file_stream->mode);
-    return file_stream;
+    SO_FILE *stream = NULL;
+    create(&stream, mode);
+    stream->descriptor = open(pathname, stream->flags, stream->mode);
+    if (stream->descriptor == SO_EOF)
+        return NULL;
+    else
+        return stream;
 }
 
-void create(SO_FILE **file_stream, char *mode)
+void create(SO_FILE **stream,const char *mode)
 {
-    *file_stream = malloc(sizeof(SO_FILE));
-    set(file_stream, mode);
-    (*file_stream)->buffer = malloc(sizeof(char) * BUFFER_SIZE);
+    *stream = malloc(sizeof(SO_FILE));
+    (*stream)->last_op = false;
+    (*stream)->start = 0;
+    (*stream)->end = 0;
+    set(stream, mode);
+    (*stream)->buffer = malloc(sizeof(char) * BUFFER_SIZE);
 }
 
-int set(SO_FILE *file_stream, char *mode)
+int set(SO_FILE *stream, char *mode)
 {
-    file_stream->mode = 0666;
+    stream->mode = 0666;
     if (is("r", mode))
-        file_stream->flags = O_RDONLY;
+        stream->flags = O_RDONLY;
     else if (is("r+", mode))
-        file_stream->flags = O_RDWR;
+        stream->flags = O_RDWR;
     else if (is("w", mode))
-        file_stream->flags = O_WRONLY | O_CREAT | O_TRUNC;
+        stream->flags = O_WRONLY | O_CREAT | O_TRUNC;
     else if (is("w+", mode))
-        file_stream->flags = O_RDWR | O_CREAT | O_TRUNC;
+        stream->flags = O_RDWR | O_CREAT | O_TRUNC;
     else if (is("a", mode))
-        file_stream->flags = O_WRONLY | O_CREAT | O_APPEND;
+        stream->flags = O_WRONLY | O_CREAT | O_APPEND;
     else if (is("a+", mode))
-        file_stream->flags = O_RDWR | O_CREAT | O_APPEND;
+        stream->flags = O_RDWR | O_CREAT | O_APPEND;
 }
 
 bool is(char *target, char *mode)
 {
     return strcmp(target, mode) == 0;
+}
+
+int so_fclose(SO_FILE *stream)
+{
+    if (stream->last_op == WRITE)
+        so_fflush(stream);
+    free(stream->buffer);
+    if (close(stream->descriptor))
+        return SO_EOF;
+    return 0;
+}
+
+int so_fileno(SO_FILE *stream)
+{
+    return stream->descriptor;
+}
+
+int so_fflush(SO_FILE *stream) // TODO: de reverificat
+{
+    if (stream->last_op == WRITE)
+    {
+        int count = stream->end - stream->start;
+        char *buffer = stream->buffer + stream->start;
+        write(stream->descriptor, buffer, count);
+    }
+}
+
+void fill(SO_FILE *stream)
+{
+    int old_pos = lseek(stream->descriptor, 0, SEEK_CUR);
+    int pos = lseek(stream->descriptor, 0, SEEK_END);
+    lseek(stream->descriptor, old_pos, SEEK_SET);
+    int bytes = (pos - old_pos) <= BUFFER_SIZE ? (pos - old_pos) : BUFFER_SIZE;
+    stream->end = read(stream->descriptor, stream->buffer, bytes);
+}
+
+int so_fgetc(SO_FILE *stream)
+{
+    if (isEmpty(stream))
+    {
+        fill(stream);
+        if (stream->end == 0)
+        {
+            return SO_EOF;
+        }
+        else if (stream->end == SO_EOF)
+        {
+            return SO_EOF;
+        }
+        else
+        {
+            stream->last_op = READ;
+            return stream->buffer[stream->start++];
+        }
+    }
+    else
+    {
+        stream->last_op = READ;
+        return stream->buffer[stream->start++];
+    }
+}
+
+bool isNotFull(SO_FILE *stream)
+{
+    return !(stream->end == BUFFER_SIZE);
+}
+
+int so_fputc(int c, SO_FILE *stream)
+{
+    if (isNotFull(stream))
+    {
+        stream->last_op = WRITE;
+        stream->start = stream->end = 0;
+        return stream->buffer[stream->end++] = c;
+    }
+    else
+    {
+        so_fflush(stream);
+        stream->last_op = WRITE;
+        return stream->buffer[stream->end++] = c;
+    }
+}
+
+size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
+{
+    return 0;
+}
+
+size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
+{
+    return 0;
+}
+
+int so_fseek(SO_FILE *stream, long offset, int whence)
+{
+    return lseek(stream->descriptor, offset, whence);
+}
+
+long so_ftell(SO_FILE *stream)
+{
+    return 0;
+}
+
+bool isEmpty(SO_FILE *stream)
+{
+    return stream->start == stream->end;
+}
+
+int so_feof(SO_FILE *stream){
+    return 0;
+}
+int so_ferror(SO_FILE *stream){
+    return 0;
+}
+
+SO_FILE *so_popen(const char *command, const char *type){
+    return NULL;
+}
+int so_pclose(SO_FILE *stream)
+{
+    return SO_EOF;
 }
--- src/utils.h
+++ src/utils.h
@@ -3,15 +3,41 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-typedef struct SO_FILE
+
+typedef struct _so_file
 {
     int descriptor;
     char *buffer;
+    int start;
+    int end;
     int mode;
     int flags;
-} SO_FILE;
+    bool last_op;
+}SO_FILE;
+
+
+#define WRITE 1
+#define READ 0
 
 #define BUFFER_SIZE 4096
 
-void create(SO_FILE **file_stream, char *pathname, char *mode);
+SO_FILE *so_fopen(const char *pathname, const char *mode);
+void create(SO_FILE **stream,const char *mode);
+int set(SO_FILE *stream, char *mode);
+bool is(char *target, char *mode);
+int so_fclose(SO_FILE *stream);
+int so_fileno(SO_FILE *stream);
+int so_fflush(SO_FILE *stream);
+
+#define SO_EOF (-1)
+void fill(SO_FILE *stream);
+int so_fgetc(SO_FILE *stream);
+bool isNotFull(SO_FILE *stream);
+int so_fputc(int c, SO_FILE *stream);
+size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream);
+size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream);
+int so_fseek(SO_FILE *stream, long offset, int whence);
+long so_ftell(SO_FILE *stream);
+bool isEmpty(SO_FILE *stream);
